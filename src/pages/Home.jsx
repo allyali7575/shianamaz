8c17f746d94b3e9dfbe3e906c38644831a11dcde
allyali7575 <allyxayn7@gmail.com>
2025-04-02 06:57:37 +0500

    Update app with recent changes

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import Fajr from "./Fajr";
 import Zuhr from "./Zuhr";
 import Asr from "./Asr";
@@ -15,31 +15,51 @@ const prayers = [
 
 export default function Home() {
     const [expanded, setExpanded] = useState(null);
+    const [isScrolled, setIsScrolled] = useState(false);
+
+    useEffect(() => {
+        const handleScroll = () => {
+            setIsScrolled(window.scrollY > 50); // Change when scrolled 50px
+        };
+
+        window.addEventListener("scroll", handleScroll);
+        return () => window.removeEventListener("scroll", handleScroll);
+    }, []);
 
     return (
-        <div className="container mt-4">
-            <h1 className="text-center mb-4">Shia Namaz App</h1>
+        <>
+            {/* <nav>
+                <a className={`navbar-brand ${isScrolled ? "scrolled" : ""
+                    }`} href="#" onClick={(e) => e.preventDefault()}>
+                    <span className="brand-1">SHIA</span>
+                    <span className="brand-2">NAMAZ</span>
+                </a>
+            </nav> */}
 
-            <div className="row justify-content-center">
+            <div className="container content">
+                <h1 className="my-4">SHIA NAMAZ APP</h1>
 
-                {prayers.map((prayer, index) => (
-                    <div className="col-12 mb-3 text-center" key={index}>
+                <div className="row justify-content-center">
 
-                        <h2 className="card prayer-card" onClick={() => setExpanded(expanded === index ? null : index)}>{prayer.name}</h2>
+                    {prayers.map((prayer, index) => (
+                        <div className="col-12 mb-3 text-center" key={index}>
 
-                        <div
-                            className={`details-cover ${expanded === index ? 'active' : ''}`}
-                        >
-                            <prayer.detailsComponent />
+                            <h2 className="card prayer-card" onClick={() => setExpanded(expanded === index ? null : index)}>{prayer.name}</h2>
+
+                            <div
+                                className={`details-cover ${expanded === index ? 'active' : ''}`}
+                            >
+                                <prayer.detailsComponent />
+                            </div>
+
                         </div>
-
-                    </div>
-                ))}
+                    ))}
 
 
 
 
+                </div>
             </div>
-        </div>
+        </>
     );
 }
